b5200e7a108b199fb3a3ce20747d1aa9ab5fab9b
David Hayes <dhayes@lalgbtcenter.org>
2020-06-30 23:45:51 -0700

    Create display

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import useRecursiveTimeout from './useRecursiveTimeout';
 // import useInterval from './useInterval';
 import * as moment from 'moment-timezone';
 import AWS from 'aws-sdk';
+import cx from 'classnames';
 import preval from 'preval.macro';
 
 const App = () => {
@@ -43,7 +44,7 @@ const App = () => {
       return;
 
     setTime(moment().toDate());
-  }, 5000);
+  }, (process.env.REACT_APP_UPDATE_FREQUENCY || 15) * 1000);
 
   // load a list of queues
   const [ queues, setQueues ] = useState(null);
@@ -73,7 +74,7 @@ const App = () => {
   }, [ ready, connect ]);
 
   // load current metric data
-  const [metrics, setMetrics] = useState([]);
+  const [metrics, setMetrics] = useState(null);
   useEffect(() => {
 
     if (!ready || queues===null)
@@ -102,7 +103,7 @@ const App = () => {
           //     'Pharmacy (SP)',
           // ],
         },
-        Groupings: [ 'QUEUE' ],
+        // Groupings: [ 'QUEUE' ],
         CurrentMetrics: [
           {
             Name: 'AGENTS_AFTER_CONTACT_WORK',
@@ -120,10 +121,10 @@ const App = () => {
             Name: 'AGENTS_NON_PRODUCTIVE',
             Unit: 'COUNT',
           },
-          {
-            Name: 'AGENTS_ON_CALL',
-            Unit: 'COUNT',
-          },
+          // {
+          //   Name: 'AGENTS_ON_CALL',
+          //   Unit: 'COUNT',
+          // },
           {
             Name: 'AGENTS_ON_CONTACT',
             Unit: 'COUNT',
@@ -146,7 +147,7 @@ const App = () => {
           },
           {
             Name: 'OLDEST_CONTACT_AGE',
-            Unit: 'SECONDS',
+            Unit: 'SECONDS', // If no grouping, gets returned in milliseconds, not seconds
           },
           {
             Name: 'SLOTS_ACTIVE',
@@ -164,32 +165,124 @@ const App = () => {
           return;
         }
         console.log({ metrics: data });
-        setMetrics(data);
-        setMsg('');
+        if (data.MetricResults.length===0) {
+          const lastUpdate = moment(data.DataSnapshotTime);
+          setMetrics(null);
+          setMsg(`No metrics available - ${lastUpdate.format('LLL')}`);
+        } else {
+          setMetrics(data);
+          setMsg('');
+        }
       });
     }
     fetchData();
 
   }, [ ready, connect, queues, time ]);
 
-  return (
-    <div className="App">
-      {msg ?
-        <header className="App-header">
+  if (msg) {
+    return (
+      <div className="App">
+        <div className="App-message">
           <img src={logo} className="App-logo" alt="logo" />
           <p>{msg}</p>
+        </div>
+      </div>
+    );
+  }
+
+  const lastUpdate = moment(metrics.DataSnapshotTime);
+  const now = moment();
+  const outdated = process.env.REACT_APP_UPDATE_OUTDATED_THRESHOLD || 15;
+  const current = lastUpdate.add(outdated, 'second').isAfter(now);
+
+  return (
+    <div className="App">
+      <div className="App-metrics">
+        <header
+          className={cx({
+            'App-header': true,
+            'App-red': !current,
+          })}
+        >
+          Last Update: {lastUpdate.format('LLL')}
         </header>
-      : 
-        <React.Fragment>
-          <div>{metrics.DataSnapshotTime.toString()}</div>
-          {metrics.MetricResults.map((metricResult, k) => {
+        <main className="App-body">
+          {[1,2,3,4].map(i => {
+
+            let metricDisplay = process.env[`REACT_APP_QUADRANT_${i}_METRIC_DISPLAY`] || '';
+            let metricDesc = process.env[`REACT_APP_QUADRANT_${i}_METRIC_DESC`] || '';
+            let metricGreen = process.env[`REACT_APP_QUADRANT_${i}_GREEN`] || '';
+            let metricYellow = process.env[`REACT_APP_QUADRANT_${i}_YELLOW`] || '';
+            let metricRed = process.env[`REACT_APP_QUADRANT_${i}_RED`] || '';
+
+            metrics.MetricResults[0].Collections.forEach(metric => {
+
+              const metricName = metric.Metric.Name;
+              let metricValue = metric.Value;
+              // There is a bug when grouping is not used, oldest contact time is returned in milliseconds
+              if (metricName==='OLDEST_CONTACT_AGE')
+                metricValue /= 60000;
+
+              metricDisplay = metricDisplay.split(metricName).join(metricValue);
+              metricDesc = metricDesc.split(metricName).join(metricValue);
+              metricGreen = metricGreen.split(metricName).join(metricValue);
+              metricYellow = metricYellow.split(metricName).join(metricValue);
+              metricRed = metricRed.split(metricName).join(metricValue);
+            });
+
+            let isGreen = false;
+            let isYellow = false;
+            let isRed = false;
+            try {
+              // eslint-disable-next-line
+              isGreen = Function(`"use strict";return (${metricGreen || 'false'});`)();
+              // eslint-disable-next-line
+              isYellow = Function(`"use strict";return (${metricYellow || 'false'});`)();
+              // eslint-disable-next-line
+              isRed = Function(`"use strict";return (${metricRed || 'false'});`)();
+            } catch (e) {
+              console.warn(e);
+              isGreen = false;
+              isYellow = false;
+              isRed = false;
+            }
+
+            // console.log({
+            //   metricDisplay,
+            //   metricDesc,
+            //   metricGreen,
+            //   metricYellow,
+            //   metricRed,
+            //   isGreen,
+            //   isYellow,
+            //   isRed,
+            // });
+
+            return (
+              <div
+                key={i} 
+                className={cx({
+                  "App-quadrant": true,
+                  "App-red": isRed,
+                  "App-yellow": isYellow && !isRed,
+                  "App-green": isGreen && !isYellow && !isYellow,
+                })}
+              >
+                <div>
+                  <p className="App-huge">{metricDisplay}</p>
+                  <p>{metricDesc}</p>
+                </div>
+              </div>
+            );
+          })}
+          {/* {metrics.MetricResults.map((metricResult, k) => {
 
             let currentQueue = null;
             if (metricResult.Dimensions)
               currentQueue = queues.QueueSummaryList.find(queue => queue.Id===metricResult.Dimensions.Queue.Id);
 
             return (
-              <div key={currentQueue.Id ? currentQueue.Id : k}>
+              <div key={currentQueue ? currentQueue.Id : k}>
                 {currentQueue ? <h2>{currentQueue.Name}</h2> : ''}
                 {metricResult.Collections.map(metric => {
                   return (
@@ -200,17 +293,17 @@ const App = () => {
                 })}
               </div>
             );
-          })}
-          <hr />
-          <b>Star Date:</b>
+          })} */}
+        </main>
+        <footer className="App-footer">
           {preval`
             const moment = require('moment-timezone');
             const now = moment().tz("America/Los_Angeles");
             const format = 'YYYY-MM-DD HH:mm:ss';
             module.exports = now.format(format);
           `}
-        </React.Fragment>
-      }
+        </footer>
+      </div>
     </div>
   );
 }
